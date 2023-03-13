edd32c5ef371c25d853c1a350bef1e6ce0cf6279
Renuka <s6revenk@uni-bonn.de>
2023-03-13 12:01:05 +0100

    Updated task_data_clean

--- src/final_project/data_management/task_data_management.py
+++ src/final_project/data_management/task_data_management.py
@@ -4,7 +4,7 @@ import pandas as pd
 import pytask
 
 from final_project.config import BLD, SRC
-from final_project.data_management import clean_data
+from final_project.data_management import clean_data, load_data
 from final_project.utilities import read_yaml
 
 
@@ -12,13 +12,26 @@ from final_project.utilities import read_yaml
     {
         "scripts": ["clean_data.py"],
         "data_info": SRC / "data_management" / "data_info.yaml",
-        "data": SRC / "data" / "data.csv",
+        "data_1": SRC / "data" / "data.csv",
     },
 )
-@pytask.mark.produces(BLD / "python" / "data" / "data_clean.csv")
+@pytask.mark.produces(BLD / "python" / "data" / "data_clean1.csv")
 def task_clean_data_python(depends_on, produces):
     """Clean the data (Python version)."""
     data_info = read_yaml(depends_on["data_info"])
-    data = pd.read_csv(depends_on["data"])
-    data = clean_data(data, data_info)
+    data_1 = pd.read_csv(depends_on["data_1"])
+    data_1 = clean_data(data_1, data_info)
+    data_1.to_csv(produces, index=False)
+
+
+@pytask.mark.depends_on(
+    {
+        "scripts": ["clean_data.py"],
+        "data": SRC / "data" / "data_raw.dta",
+    },
+)
+@pytask.mark.produces(BLD / "python" / "data" / "data_clean.csv")
+def task_clean_data(depends_on, produces):
+    """Clean the data (Python version)."""
+    data = load_data(depends_on["data"])
     data.to_csv(produces, index=False)
