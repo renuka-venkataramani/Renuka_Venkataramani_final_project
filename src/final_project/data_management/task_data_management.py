e2241fc5b824f9c87aaef31e2e9d5b01c7640e5d
Renuka <s6revenk@uni-bonn.de>
2023-03-31 00:14:43 +0200

    Added task to create outcome variables dataset

--- src/final_project/data_management/task_data_management.py
+++ src/final_project/data_management/task_data_management.py
@@ -1,10 +1,12 @@
 """Tasks for managing the data."""
 
-import pandas as pd
 import pytask
 
 from final_project.config import BLD, SRC
-from final_project.data_management import clean_data, load_data
+from final_project.data_management import (
+    build_dataset,
+    load_data,
+)
 from final_project.utilities import read_yaml
 
 
@@ -18,20 +20,22 @@ from final_project.utilities import read_yaml
 @pytask.mark.produces(BLD / "python" / "data" / "data_clean1.csv")
 def task_clean_data_python(depends_on, produces):
     """Clean the data (Python version)."""
-    data_info = read_yaml(depends_on["data_info"])
-    data_1 = pd.read_csv(depends_on["data_1"])
-    data_1 = clean_data(data_1, data_info)
+    read_yaml(depends_on["data_info"])
+    data_1 = load_data(depends_on["data_1"])
     data_1.to_csv(produces, index=False)
 
 
 @pytask.mark.depends_on(
     {
         "scripts": ["clean_data.py"],
+        "data_info": SRC / "data_management" / "data_info.yaml",
         "data": SRC / "data" / "data_raw.dta",
     },
 )
-@pytask.mark.produces(BLD / "python" / "data" / "data_clean.csv")
-def task_clean_data(depends_on, produces):
+@pytask.mark.produces(BLD / "python" / "data" / "outcome_variables_data.csv")
+def task_build_dataset(depends_on, produces):
     """Clean the data (Python version)."""
     data = load_data(depends_on["data"])
-    data.to_csv(produces, index=False)
+    data_info = read_yaml(depends_on["data_info"])
+    outcome_variables_data = build_dataset(data=data, data_info=data_info)
+    outcome_variables_data.to_csv(produces, index=False)
