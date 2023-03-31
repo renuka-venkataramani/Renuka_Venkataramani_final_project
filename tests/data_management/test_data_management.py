dc3dd415155d743fd0504c14d5c60486893bafe0
Renuka <s6revenk@uni-bonn.de>
2023-03-31 19:16:32 +0200

    Added select_partial_data test function

--- tests/data_management/test_data_management.py
+++ tests/data_management/test_data_management.py
@@ -1,6 +1,14 @@
 import pytest
 from final_project.config import TEST_DIR
+from final_project.data_management import (
+    select_partial_data,
+)
 from final_project.utilities import read_yaml
+
+
+@pytest.fixture()
+def data():
+    return pd.read_csv(TEST_DIR / "data_management" / "test_data.csv")
 
 
 @pytest.fixture()
@@ -8,7 +16,9 @@ def data_info():
     return read_yaml(TEST_DIR / "data_management" / "data_info_fixture.yaml")
 
 
-def test_load_data(file_path):
-    with pytest.raises(ValueError):
-        file_path = "Examplefile/path/sample.pkl"
-        assert load_data(file_path), "The covariance matrix should be positive definite"
+def test_select_partial_data(data, variable_list):
+    x = select_partial_data(
+        data=data,
+        variable_list=["bankdens_1920", "farmout_1860", "ShNewSkills1940"],
+    )
+    assert x.columns.to_list() == ["bankdens_1920", "farmout_1860", "ShNewSkills1940"]
