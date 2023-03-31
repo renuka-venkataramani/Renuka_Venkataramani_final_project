dbe2ae9ec0fba882a5e52f83e267c9d7792a7170
Renuka <s6revenk@uni-bonn.de>
2023-03-31 22:06:09 +0200

    Final version: Added doctstings

--- src/final_project/data_management/clean_data.py
+++ src/final_project/data_management/clean_data.py
@@ -27,6 +27,16 @@ def load_data(file_path):
 
 
 def select_partial_data(data, variables_list):
+    """To select a part of the data from the main data set.
+
+    Args:
+        data (pd.DataFrame): DataFrame from which subset data should be extracted
+        variables_list (list): List of columns to be included in the dataset
+
+    Returns:
+        pd.DataFrame: a partial subset of the main DataFrame
+
+    """
     outcome_variables_data = pd.DataFrame()
     for col_name in variables_list:
         outcome_variable = data.loc[:, data.columns.str.contains(col_name)]
@@ -38,11 +48,16 @@ def select_partial_data(data, variables_list):
 
 
 def build_dataset(data, data_info):
-    """
+    """To build outcome/dependent variables dataset. Renames all the columns to
+    meaningful names.
 
     Args:
-        data (_type_): _description_
-        data_info (_type_): _description_.
+        data (pd.DataFrame): raw_file that needs to be cleaned
+        data_info (.yaml file): contains the name of the dictionaries used to rename the columns.
+
+    Returns:
+        pd.DataFrame: Returns datframe containing all the dependent variable used in the anlaysis
+
     """
     # keep columns present in data_info['outcome_variables']
     cross_sectional_data = select_partial_data(
@@ -69,7 +84,18 @@ def build_dataset(data, data_info):
 
 
 def _drop_nan(data, data_info):
-    """"""
+    """It is a sub function as a part of build_dataset func. Cannot be used
+    individually. Removes nan values if present in all the rows, and drops unwanted
+    columns.
+
+    Args:
+        data (pd.DataFrame): DataFrame to be cleaned
+        data_info (.yaml): Contains list and dictionaries with information about the data
+
+    Returns:
+        pd.Dataframe: Returns cleaned dataframe
+
+    """
     for col in data.filter(like="state_").columns.to_list():
         data = data.drop(columns=col)
     data = data.drop(columns=data_info["drop_outcome_variables"])
@@ -77,6 +103,14 @@ def _drop_nan(data, data_info):
 
 
 def _change_column_names(custom_rename_func):
+    """Func used to rename timeseries column names with '_' in between and a year at
+    last.
+
+    Args:
+        custom_rename_func (func reference): user-defined func given as an input to the main func. Func uses decorators
+
+    """
+
     def _rename_columns(data_subset, data_info):
         change_col_name = (
             data_subset.filter(like="_")
@@ -96,6 +130,17 @@ def _change_column_names(custom_rename_func):
 
 @_change_column_names
 def _outcome_variables_rename(data_subset, data_info):
+    """A function that makes changes to the _change_column_names func added as a
+    decorator.
+
+    Args:
+        data_subset (dataframe): Subset that contains 'DivMf' columns
+        data_info (.yaml): Contains list and dictionaries with information about the data
+
+    Returns:
+        Dataframe : Dataframe with DivMf columns appropriately renamed
+
+    """
     rename_DivMf_col = (
         "manufacturing_diversity"
         + "_"
@@ -106,6 +151,17 @@ def _outcome_variables_rename(data_subset, data_info):
 
 
 def build_geoclimatic_controls(data, var, data_info):
+    """Builds dataset containing geoclimatic control variables.
+
+    Args:
+        data (dataframe): The source data
+        var (dict): dictionary taken from data_info that contains rename keys and values
+        data_info (.yaml): Description of the columns in data
+
+    Returns:
+        Dataframe: Geoclimatic dataframe
+
+    """
     geoclimatic_data = select_partial_data(data=data, variables_list=var.keys())
     for key, value in var.items():
         geoclimatic_data.columns = geoclimatic_data.columns.str.replace(key, value)
@@ -118,6 +174,16 @@ def build_geoclimatic_controls(data, var, data_info):
 
 
 def build_socioeconomic_controls(data, data_info):
+    """Build socioeconomic control variables datasets.
+
+    Args:
+        data (pd.DataFrame): Socioeconomic DataFrame to be cleaned
+        data_info (.yaml): Contains list and dictionaries with information about the data
+
+    Returns:
+        dataframe: returns dataframe containing socioeconomic controls
+
+    """
     socioeconomic_controls = select_partial_data(
         data=data,
         variables_list=data_info["socioeconomic_controls_rename"].keys(),
@@ -130,6 +196,16 @@ def build_socioeconomic_controls(data, data_info):
 
 
 def build_crop_specific_controls(sub_data):
+    """Build crop-specific control variables datasets.
+
+    Args:
+        data (pd.DataFrame): crop-specific DataFrame to be cleaned
+        data_info (.yaml): Contains list and dictionaries with information about the data
+
+    Returns:
+        dataframe: returns dataframe containing socioeconomic controls
+
+    """
     crop_specific_controls = select_partial_data(
         data=sub_data,
         variables_list=sub_data.filter(like="dominant_").columns.to_list(),
