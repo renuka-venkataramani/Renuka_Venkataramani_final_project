a745197b85dcd020b8b3382a486509eaa4ef78b2
Renuka <s6revenk@uni-bonn.de>
2023-03-31 17:51:27 +0200

    Final Version

--- src/final_project/data_management/__init__.py
+++ src/final_project/data_management/__init__.py
@@ -1,5 +1,23 @@
 """Functions for managing data."""
 
-from final_project.data_management.clean_data import clean_data
+from final_project.data_management.clean_data import (
+    build_crop_specific_controls,
+    build_dataset,
+    build_geoclimatic_controls,
+    build_socioeconomic_controls,
+    load_data,
+    select_partial_data,
+)
+from final_project.data_management.variable_construction import (
+    construct_control_variables_dataset,
+)
 
-__all__ = [clean_data]
+__all__ = [
+    build_dataset,
+    load_data,
+    construct_control_variables_dataset,
+    select_partial_data,
+    build_geoclimatic_controls,
+    build_socioeconomic_controls,
+    build_crop_specific_controls,
+]
