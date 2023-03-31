2cfc930982ad648ee9506b4dbc545ec4ca7eeed8
Renuka <s6revenk@uni-bonn.de>
2023-03-31 14:55:03 +0200

    Added task to build geoclimatic, cross-specific and socio-economic dataset

--- src/final_project/data_management/task_data_management.py
+++ src/final_project/data_management/task_data_management.py
@@ -4,7 +4,11 @@ import pytask
 
 from final_project.config import BLD, SRC
 from final_project.data_management import (
+    build_crop_specific_controls,
     build_dataset,
+    build_geoclimatic_controls,
+    build_socioeconomic_controls,
+    construct_control_variables_dataset,
     load_data,
 )
 from final_project.utilities import read_yaml
@@ -33,10 +37,45 @@ def task_build_dataset(depends_on, produces):
         "data": SRC / "data" / "data_to_construct_variables.dta",
     },
 )
-@pytask.mark.produces(BLD / "python" / "data" / "geoclimatic_controls.csv")
-def task_construct_control_variables(depends_on, produces):
+@pytask.mark.produces(BLD / "python" / "data" / "constructed_variables.csv")
+def task_constructed_variables(depends_on, produces):
     """Clean the data (Python version)."""
     data = load_data(depends_on["data"])
     data_info = read_yaml(depends_on["data_info"])
     outcome_variables_data = construct_control_variables_dataset(data, data_info)
     outcome_variables_data.to_csv(produces, index=False)
+
+
+@pytask.mark.depends_on(
+    {
+        "scripts": ["variable_construction.py"],
+        "data_info": SRC / "data_management" / "data_info.yaml",
+        "constructed_data": BLD / "python" / "data" / "constructed_variables.csv",
+        "data": SRC / "data" / "data_raw.dta",
+    },
+)
+@pytask.mark.produces(
+    {
+        "geoclimatic": BLD / "python" / "data" / "geoclimatic_control_variables.csv",
+        "socioeconomic": BLD
+        / "python"
+        / "data"
+        / "socioeconomic_control_variables.csv",
+        "crop_specific": BLD / "python" / "data" / "cropspecific_control_variables.csv",
+    },
+)
+def task_build_control_variables(depends_on, produces):
+    """Clean the data (Python version)."""
+    data = load_data(depends_on["data"])
+    constructed_data = load_data(depends_on["constructed_data"])
+    data_info = read_yaml(depends_on["data_info"])
+    geoclimatic_controls_data = build_geoclimatic_controls(
+        data,
+        var=data_info["geoclimatic_controls_column_rename_mapping"],
+        data_info=data_info,
+    )
+    socioeconomic_controls = build_socioeconomic_controls(data, data_info)
+    crop_specific_controls = build_crop_specific_controls(sub_data=constructed_data)
+    geoclimatic_controls_data.to_csv(produces["geoclimatic"], index=False)
+    socioeconomic_controls.to_csv(produces["socioeconomic"], index=False)
+    crop_specific_controls.to_csv(produces["crop_specific"], index=False)
