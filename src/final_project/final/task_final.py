08a7afe0e79d4c2cbb183849c4cd09e1afed3c19
Renuka <s6revenk@uni-bonn.de>
2023-04-01 05:59:27 +0200

    Added task file to plot the graph

--- src/final_project/final/task_final.py
+++ src/final_project/final/task_final.py
@@ -1,8 +1,10 @@
 """Tasks running the results formatting (tables, figures)."""
+import geopandas as gpd
 import pytask
 
-from final_project.config import BLD, SRC
+from final_project.config import BLD, GROUP, SRC
 from final_project.data_management import load_data
+from final_project.final.plot import map_agriculture_diversity
 from final_project.final.tables import create_table_1_agricultural_production_data
 from final_project.utilities import read_yaml
 
@@ -35,3 +37,32 @@ def task_table_1_agricultural_production_data(depends_on, produces):
         data_info,
     )
     outcome_variables_data.to_pickle(produces)
+
+
+for group in GROUP:
+
+    kwargs = {
+        "group": group,
+        "produces": BLD / "python" / "figures" / f"smoking_by_{group}.png",
+    }
+
+    @pytask.mark.depends_on(
+        {
+            "scripts": ["plot.py"],
+            "data": BLD / "python" / "data" / "outcome_variables_data.csv",
+            "shape_file": SRC / "data" / "counties_shapefile.dbf",
+            "data_info": SRC / "data_management" / "data_info.yaml",
+        },
+    )
+    @pytask.mark.task(id=group, kwargs=kwargs)
+    def task_map1_agriculture_diversity(depends_on, group, produces):
+        """Clean the data (Python version)."""
+        shp_data = gpd.read_file(depends_on["shape_file"])
+        data = load_data(depends_on["data"])
+        read_yaml(depends_on["data_info"])
+        map1_agri_div = map_agriculture_diversity(
+            csv_data=data,
+            geo_data=shp_data,
+            variable=group,
+        )
+        map1_agri_div.write_image(produces)
